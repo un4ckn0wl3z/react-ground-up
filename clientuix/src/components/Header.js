f1802f0c6d50b1a4d032d13a84e81f4f92f675ae
un4ckn0wl3z <slickerth@gmail.com>
2019-10-13 16:41:18 +0700

    complete intro

--- clientuix/src/components/Header.js
+++ clientuix/src/components/Header.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = () => {
-    return <h1>HEADER</h1>
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('constructor| ', props);
+        this.state = {
+            date: new Date()
+        };
+        console.log('constructor|X< ');
+    }
+
+    componentDidMount() {
+        this.timerId =  setInterval(() => {
+            this.tick();
+        }, 1000);
+    }
+
+    componentDidUpdate() {
+
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.date.toLocaleTimeString()}</h1>
+            </div>
+        )
+    }
 }
 
 export default Header;
