a84aa4816874bf79b83b59e4842bf358b42d0edf
un4ckn0wl3z <slickerth@gmail.com>
2019-10-13 13:59:11 +0700

    ..

--- modern-js/client/default-param.js
+++ modern-js/client/default-param.js
@@ -9,3 +9,10 @@ howAreYou('Sad');
 
 let [x, y = 0] = [1];
 console.log(x, y);
+
+let [j = 9, k = 8] = [];
+console.log(x, y);
+console.log(j, k);
+
+const [{ prop: t = 999 }] = [{ prop: undefined}];
+console.log(t);
