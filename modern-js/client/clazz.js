23744fd5bfd4cfc50c0ddb9073cff93adafeef98
un4ckn0wl3z <slickerth@gmail.com>
2019-10-13 14:13:24 +0700

    add extends

--- modern-js/client/clazz.js
+++ modern-js/client/clazz.js
@@ -21,9 +21,23 @@ class Rectangle {
     }
 }
 
+
+class Square extends Rectangle {
+    constructor(width) {
+        super(width, width);
+    }
+}
+
+
 const r = new Rectangle(100, 200);
 console.log(r.height, r.width);
 r.color = 'RED';
 console.log(r.color);
 console.log(r.area);
 console.log(Rectangle.areas(r));
+
+
+const s = new Square(10);
+s.color = 'BLUE';
+console.log(s.color);
+console.log(s.area);
