232cb9131831226f5f1da24d7375268a09d7226f
un4ckn0wl3z <slickerth@gmail.com>
2019-10-13 14:36:42 +0700

    play with array

--- modern-js/client/array-func.js
+++ modern-js/client/array-func.js
@@ -1,1 +1,19 @@
 // array
+let fruit = ['Apple', 'Banana'];
+console.log(fruit.length);
+console.log(fruit[0]);
+console.log(fruit[1]);
+
+// loop
+fruit.forEach((item, index, arr) => {
+    console.log(item, index);
+
+});
+
+// push
+fruit.push('Orange');
+console.log(fruit);
+
+// pop
+fruit.pop('Orange');
+console.log(fruit);
