3fb0f6302de8dd3ac975b60adaea2ea2293c4996
un4ckn0wl3z <slickerth@gmail.com>
2019-10-13 16:07:10 +0700

    simple functional component

--- clientuix/src/App.js
+++ clientuix/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-
-
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    )
+  };
 }
 
 export default App;
--- clientuix/src/index.js
+++ clientuix/src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-function Welcome (){
-    return <h1>Welcome to React</h1>;
-}
 
 
-
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
