1a7a97e7e1d85b70d641bdcf2b09155c45f736e2
un4ckn0wl3z <slickerth@gmail.com>
2019-10-13 14:07:00 +0700

    add get set

--- modern-js/client/clazz.js
+++ modern-js/client/clazz.js
@@ -3,7 +3,17 @@ class Rectangle {
         this.height = height;
         this.width = width;
     }
+
+    get color() {
+        return this._color;
+    }
+
+    set color(c) {
+        this._color = c;
+    }
 }
 
 const r = new Rectangle(100, 200);
 console.log(r.height, r.width);
+r.color = 'RED';
+console.log(r.color);
