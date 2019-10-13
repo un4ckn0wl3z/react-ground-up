666d6c5bc80860d2fbf74f1b4a48b91d4c65c3d3
un4ckn0wl3z <slickerth@gmail.com>
2019-10-13 14:08:51 +0700

    add static method

--- modern-js/client/clazz.js
+++ modern-js/client/clazz.js
@@ -11,9 +11,18 @@ class Rectangle {
     set color(c) {
         this._color = c;
     }
+
+    get area() {
+        return this.height * this.width;
+    }
+
+    static areas(obj) {
+        return obj;
+    }
 }
 
 const r = new Rectangle(100, 200);
 console.log(r.height, r.width);
 r.color = 'RED';
 console.log(r.color);
+console.log(r.area);
