19bc8e28f86fcf55cdbc543238041d3e3fe0568d
un4ckn0wl3z <slickerth@gmail.com>
2019-10-13 14:22:24 +0700

    add arrow

--- modern-js/client/arrow-func.js
+++ modern-js/client/arrow-func.js
@@ -4,6 +4,10 @@ let greet1 = function (fname, lname) {
     return fname + ' ' + lname;
 }
 
-console.log(greet1('Anwuat','K.'));
+console.log(greet1('Anwuat', 'K.'));
 
 
+let greet2 = (fname, lname) => {
+    return fname + ' ' + lname;
+}
+console.log(greet2('Anwuat', 'K.'));
