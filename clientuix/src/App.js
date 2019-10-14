e8b28321525d2ec685e6a8fd3dd88c2d352a2bd3
un4ckn0wl3z <slickerth@gmail.com>
2019-10-14 14:26:33 +0700

    add http client and db and json-server

--- clientuix/src/App.js
+++ clientuix/src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Monitor from './components/monitor/Monitor';
+import axios from 'axios';
+
 class App extends Component {
 
   constructor(props) {
@@ -13,16 +15,36 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      products: [
-        { productId: 1, productName: "สลัดผัก", unitPrice: "120", thumbnail: "/images/product/1.jpg" },
-        { productId: 2, productName: "ไก่ทอด", unitPrice: "90", thumbnail: "/images/product/2.jpg" },
-        { productId: 3, productName: "บิงซู", unitPrice: "200", thumbnail: "/images/product/3.jpg" },
-        { productId: 4, productName: "เฟรนฟราย", unitPrice: "140", thumbnail: "/images/product/4.jpg" },
-        { productId: 5, productName: "เค้ก 3 ชั้น", unitPrice: "200", thumbnail: "/images/product/5.jpg" },
-        { productId: 6, productName: "กาแฟ เฮลตี้ฟู้ด", unitPrice: "140", thumbnail: "/images/product/6.jpg" }
-      ]
+    // this.setState({
+    //   products: [
+    //     { productId: 1, productName: "สลัดผัก", unitPrice: "120", thumbnail: "/images/product/1.jpg" },
+    //     { productId: 2, productName: "ไก่ทอด", unitPrice: "90", thumbnail: "/images/product/2.jpg" },
+    //     { productId: 3, productName: "บิงซู", unitPrice: "200", thumbnail: "/images/product/3.jpg" },
+    //     { productId: 4, productName: "เฟรนฟราย", unitPrice: "140", thumbnail: "/images/product/4.jpg" },
+    //     { productId: 5, productName: "เค้ก 3 ชั้น", unitPrice: "200", thumbnail: "/images/product/5.jpg" },
+    //     { productId: 6, productName: "กาแฟ เฮลตี้ฟู้ด", unitPrice: "140", thumbnail: "/images/product/6.jpg" }
+    //   ]
+    // });
+
+
+    // fetch('http://localhost:3001/products', {
+    //   method: 'GET'
+    // }).then(res => {
+    //   return res.json();
+    // }).then(data => {
+    //   this.setState({
+    //     products: data
+    //   });
+    // });
+
+
+    axios.get('http://localhost:3001/products').then(res => {
+      this.setState({
+        products: res.data
+      });
     });
+
+
   }
 
   render() {
