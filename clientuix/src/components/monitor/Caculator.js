ce78df8ab09ce1b5eec4ab9c5056e88153045b07
un4ckn0wl3z <slickerth@gmail.com>
2019-10-14 14:39:53 +0700

    post order to backend api

--- clientuix/src/components/monitor/Caculator.js
+++ clientuix/src/components/monitor/Caculator.js
@@ -20,7 +20,7 @@ class Caculator extends Component {
     }
 
     render() {
-        const { totalPrice, orders } = this.props;
+        const { totalPrice, orders, onCancelOrder, onConfirmOrder } = this.props;
         return (
             <div>
                 <h1 className="text-right">{totalPrice}</h1>
@@ -29,11 +29,16 @@ class Caculator extends Component {
                     {this.showOrders(orders)}
                 </ul>
                 <hr />
-                <button className="btn btn-block btn-danger title">Confirm</button>
-                <button className="btn btn-block btn-secondary title">Cancel</button>
+                <button onClick={() => {
+                    onConfirmOrder();
+                }} className="btn btn-block btn-danger title">Confirm</button>
+                <button onClick={() => {
+                    onCancelOrder();
+                }} className="btn btn-block btn-secondary title">Cancel</button>
             </div>
         );
     }
 }
+
 
 export default Caculator;
--- clientuix/src/components/monitor/Monitor.js
+++ clientuix/src/components/monitor/Monitor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Caculator from './Caculator';
 import ProductList from '../product/ProductList';
+import axios from 'axios';
 
 class Monitor extends Component {
 
@@ -12,6 +13,8 @@ class Monitor extends Component {
         }
         this.addOrder = this.addOrder.bind(this);
         this.delOrder = this.delOrder.bind(this);
+        this.cancelOrder = this.cancelOrder.bind(this);
+        this.confirmOrder = this.confirmOrder.bind(this);
 
     }
 
@@ -44,6 +47,27 @@ class Monitor extends Component {
         console.log(this.state.orders)
     }
 
+    cancelOrder() {
+        this.setState({
+            totalPrice: 0,
+            orders: []
+        });
+    }
+
+    confirmOrder() {
+        const { totalPrice, orders } = this.state;
+        axios.post('http://localhost:3001/orders', {
+            orderDate: new Date(),
+            totalPrice,
+            orders
+        }).then(res => {
+            this.setState({
+                totalPrice: 0,
+                orders: []
+            });
+        });
+    }
+
     render() {
         return (
             <div className="container-fluid">
@@ -52,7 +76,7 @@ class Monitor extends Component {
                         <ProductList products={this.props.products} onAddOrder={this.addOrder} />
                     </div>
                     <div className="col-md-3">
-                        <Caculator totalPrice={this.state.totalPrice} orders={this.state.orders} onDeleteOrder={this.delOrder} />
+                        <Caculator totalPrice={this.state.totalPrice} orders={this.state.orders} onDeleteOrder={this.delOrder} onCancelOrder={this.cancelOrder} onConfirmOrder={this.confirmOrder} />
                     </div>
                 </div>
             </div>
